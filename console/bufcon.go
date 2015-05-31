f697e2d09abc1cf0ec5850977c68ad9f076475fa
ionous <github@ionous.net>
2015-05-30 21:45:08 -0700

    add missing bufcon.go file
    ( something about the git submit caused it to go missing. )

---
+++ console/bufcon.go
@@ -0,0 +1,28 @@
+package console
+
+//
+// Creates a console whose input comes from the passed strings.
+// when the strings are exhausted the Readln() returns false.
+//
+func NewBufCon(strs []string) *BufCon {
+	return &BufCon{strs: strs}
+}
+
+//
+type BufCon struct {
+	strs  []string
+	index int
+	BufferedOutput
+}
+
+//
+// Returns the next input string, false when input has been exhausted.
+//
+func (this *BufCon) Readln() (ret string, okay bool) {
+	okay = this.index < len(this.strs)
+	if okay {
+		ret = this.strs[this.index]
+		this.index++
+	}
+	return ret, okay
+}
