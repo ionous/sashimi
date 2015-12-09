00612c46b0d09eb6953dfaf4df80f42f996f1b09
ionous <github@ionous.net>
2015-12-08 16:18:55 -0800

    minor: use "fact" as var name in "facts" module; not "quip"

--- extension/facts/native/memory.go
+++ extension/facts/native/memory.go
@@ -16,26 +16,26 @@ func PlayerMemory(g G.Play) Memory {
 	return Memory{g}
 }
 
-func (m Memory) TriesToLearn(quip G.IObject) (newlyLearned bool) {
+func (m Memory) TriesToLearn(fact G.IObject) (newlyLearned bool) {
 	// return newlyLearned
-	if recollects := quip.Is("recollected"); !recollects {
-		quip.IsNow("recollected")
+	if recollects := fact.Is("recollected"); !recollects {
+		fact.IsNow("recollected")
 		newlyLearned = true
 	}
 	return
 }
 
-// LearnQuip causes actors to recollect the passed quip.
+// LearnQuip causes actors to recollect the passed fact.
 // we can use this for facts for now too
 // mostly the player will need this -- so just a table with precese is enough
 // but it could also be actor, id
-func (m Memory) Learns(quip G.IObject) {
-	//m.AppendObject(quip)
-	quip.IsNow("recollected")
+func (m Memory) Learns(fact G.IObject) {
+	//m.AppendObject(fact)
+	fact.IsNow("recollected")
 }
 
-// RecollectsQuip determines if the passed quip has been spoken.
-func (m Memory) Recollects(quip G.IObject) bool {
-	//return m.Contains(quip)
-	return quip.Is("recollected")
+// RecollectsQuip determines if the passed fact has been spoken.
+func (m Memory) Recollects(fact G.IObject) bool {
+	//return m.Contains(fact)
+	return fact.Is("recollected")
 }
