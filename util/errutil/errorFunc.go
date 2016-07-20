0a712e9f2a6519dfc10484bf061b1b82935ce1db
ionous <github@ionous.net>
2016-07-20 15:01:07 -0700

    comments

--- util/errutil/errorFunc.go
+++ util/errutil/errorFunc.go
@@ -1,13 +1,9 @@
 package errutil
 
-//
-// ErrorFunc implements the go "error" interface for parameterless functions, typically closures.
-//
+// ErrorFunc implements the go Error() interface for parameterless functions, typically closures.
 type Func func() string
 
-//
-// Call the function and return a string as per the go "error" interface.
-//
-func (this Func) Error() string {
-	return this()
+// Call the function and return a string as per the go Error() interface.
+func (e Func) Error() string {
+	return e()
 }
