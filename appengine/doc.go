e6fc4be50b7eedca3ab7775478500ac300195d2a
ionous <github@ionous.net>
2016-10-17 22:22:45 -0700

    remove appengine from mars

--- appengine/doc.go
+++
@@ -1,8 +0,0 @@
-// Package appengine is an attempt at running sashimi on google's appengine
-// It works, but is slow. Long term, removing the startup parsing: instead an pipeline to compile into data; and moving to proper queries would help.
-// example:
-// run gen.go in sash-alice/gen to build/extract the source
-// run "goapp serve demo" in sash-alice
-// goapp serve demo.
-// http://localhost:8000/datastore
-package appengine
